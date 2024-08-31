153bcdc4171407dcefe4552b1c9864737557c03f
Дмитрий Голов <dgolov@mts.ru>
2024-09-01 00:50:25 +0300

    fixed get_peer_file.sh

--- get_peer_file.sh
+++ get_peer_file.sh
@@ -1,1 +1,10 @@
-_
+if [ "$#" -ne 1 ]; then
+    echo "Using: $0 <number_peer>"
+    exit 1
+fi
+
+PEER_NUMBER=$1
+
+echo "Loading peer: $PEER_NUMBER file"
+
+cat ~/docker/wireguard/config/peer$PEER_NUMBER/peer$PEER_NUMBER.conf
